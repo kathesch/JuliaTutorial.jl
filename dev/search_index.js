be3725476f3625c96486c549c3cfa38f0ff17f80
Documenter.jl <documenter@juliadocs.github.io>
2022-09-22 20:37:16 +0000

    build based on 6fa7293

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#JuliaTutorial.jl","page":"JuliaTutorial.jl","title":"JuliaTutorial.jl","text":"","category":"section"},{"location":"","page":"JuliaTutorial.jl","title":"JuliaTutorial.jl","text":"Documentation for JuliaTutorial.jl","category":"page"}]
+[{"location":"#JuliaTutorial.jl","page":"JuliaTutorial.jl","title":"JuliaTutorial.jl","text":"","category":"section"},{"location":"","page":"JuliaTutorial.jl","title":"JuliaTutorial.jl","text":"Documentation for JuliaTutorial.jl.","category":"page"},{"location":"","page":"JuliaTutorial.jl","title":"JuliaTutorial.jl","text":"Welcome to the page!","category":"page"}]
 }
