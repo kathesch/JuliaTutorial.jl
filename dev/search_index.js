292f35dcc96faf8848dd184d971000005da264b4
Documenter.jl <documenter@juliadocs.github.io>
2022-09-22 20:25:21 +0000

    build based on 2be6a24

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#JuliaTutorial.jl","page":"JuliaTutorial.jl","title":"JuliaTutorial.jl","text":"","category":"section"},{"location":"","page":"JuliaTutorial.jl","title":"JuliaTutorial.jl","text":"Documentation for JuliaTutorial.jl","category":"page"}]
+}
